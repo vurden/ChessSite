68fc6cadf367b8f05a4dcf8c35d4b48bc6c4334d
vurden <123vurden456@gmail.com>
2021-12-07 14:09:35 -0500

    pawnRook working

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var passSave;
 var pass;
 var global;
 var turn = "white";
+var opposite = "black";
 var tiles = [
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", 
     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
@@ -33,7 +34,6 @@ function freeplay() {
 }
 
 function go(event) {
-    console.log("GOGOGOGOGOO")
     global = event.target.id
     pass;
     passSave = pass;
@@ -46,9 +46,7 @@ function go(event) {
 
     //get grids peice can move to
     var x = whatFun(info[1]);
-    console.log(x);
     pass = peices[x](info[2]);
-    console.log(pass)
     
     //removes event listeners for highlighted tiles
     removeEvents(passSave)
@@ -79,8 +77,10 @@ function move(event) {
     removeStartTurn()
     if (turn == "white") {
         turn = "black";
+        opposite = "white";
     } else if (turn == "black") {
         turn = "white";
+        opposite = "black";
     }
     startTurn();
     
@@ -98,7 +98,6 @@ function defaultColor() {
 }
 
 function startTurn() {
-    console.log(turn)
     tiles.forEach(element => {
         var info = details(document.getElementById(element));
         if (info == undefined) {
@@ -177,32 +176,36 @@ const peices = [
             }
         }
         x = another(place, 9);
-        console.log(right(x))
-        if (x[0] == "black" && right(x)) {
+        if (x[0] == "black") {
             array.push(x[2]);
         }
         x = another(place, 7);
-        if (x[0] == "black" && left(x)) {
+        if (x[0] == "black") {
             array.push(x[2]);
         }
         return array;
     },
     function rookW(place) {
         console.log("White Rook")
-        console.log(place)
-        return north(place, 8)
+        var array = [];
+        array = north(place, 8, array)
+        array = north(place, -8, array)
+        array = north(place, 1, array)
+        array = north(place, -1, array)
+        return array;
+        
         
         //document.getElementById(tiles[another(current, 8)[4]])
         
 
     },
-    function knightW() {
+    function knightW(place) {
         console.log("White Knight")
-    },function bishopW() {
+    },function bishopW(place) {
         console.log("White Bishop")
-    },function kingW() {
+    },function kingW(place) {
         console.log("White King")
-    },function queenW() {
+    },function queenW(place) {
         console.log("White Queen")
     },
     
@@ -219,39 +222,45 @@ const peices = [
             }
         }
         x = another(place, -7);
-        if (x[0] == "white" && right(x)) {
+        if (x[0] == "white") {
             array.push(x[2]);
         }
         x = another(place, -9);
-        if (x[0] == "white" && left(x)) {
+        if (x[0] == "white") {
             array.push(x[2]);
         }
         return array;
-    },function rookB() {
+    },function rookB(place) {
         console.log("Black Rook")
-    },function knightB() {
+        var array = [];
+        array = north(place, 8, array)
+        array = north(place, -8, array)
+        array = north(place, 1, array)
+        array = north(place, -1, array)
+        return array;
+        
+    },function knightB(place) {
         console.log("Black Knight")
-    },function bishopB() {
+    },function bishopB(place) {
         console.log("Black Bishop")
-    },function kingB() {
+    },function kingB(place) {
         console.log("Black King")
-    },function queenB() {
+    },function queenB(place) {
         console.log("Black Queen")
     },
 
 ]
 
-function diagonal(place, team) {}
-
-function north(place, move) {
-    var array = [];
+function north(place, move, array) {
     var current = place;  
+    
     while (true) {
-        if (another(current, move)[0] == "none" && toppom(another(current, move))) {
-            var x = another(current, 8);
+        if (another(current, move)[0] == "none") {
+            var x = another(current, move);
             array.push(x[2]);
             current = x[2]
-        } else if (another(current, move)[0] == "black" && toppom(another(current, move))) {
+            if (tiles.indexOf(current) > 56) {break;}
+        } else if (another(current, move)[0] == opposite) {
             var x = another(current, move);
             array.push(x[2]);
             break;
@@ -262,6 +271,19 @@ function north(place, move) {
 }
 
 function another(place, x) {
+    if (tiles.indexOf(place) + x > 63) {
+        return ["na"];
+    } else if (tiles.indexOf(place) + x < 0) {
+        return ["na"];
+    } else if (x == 9 || x == -7 || x == 1) {
+        if (right(tiles.indexOf(place))) {
+            return ["na"];
+        }
+    } else if (x == 7 || x == -9 || x == -1) {
+        if (left(tiles.indexOf(place))) {
+            return ["na"];
+        }
+    }
     var push = details(document.getElementById(tiles[tiles.indexOf(place) + x]));
     push.push(tiles.indexOf(place));
     push.push(tiles.indexOf(place) + x);
@@ -269,15 +291,16 @@ function another(place, x) {
 }
 
 function left(x) {
-    return [0, 8, 16, 24, 32, 40, 48, 56].every((item) => {return item!=x[3]})
+    return ![0, 8, 16, 24, 32, 40, 48, 56].every((item) => {return item!=x})
 }
 function right(x) {
-    return [7, 15, 23, 31, 39, 47, 55, 63].every((item) => {return item!=x[3]})
+    return ![7, 15, 23, 31, 39, 47, 55, 63].every((item) => {return item!=x})
 }
 function bottom(x) {
     return [0, 1, 2, 3, 4, 5, 6, 7].every((item) => {return item!=x[3]})
 }
 function toppom(x) {
     //return x[3] < 64
-    return [56, 57, 58, 59, 60, 61, 62, 63].every((item) => {return item!=x[3]})
+    //return [56, 57, 58, 59, 60, 61, 62, 63].every((item) => {return item!=x[3]})
+    if (x => 56) {return false}
 }
